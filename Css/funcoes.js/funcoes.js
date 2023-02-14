597c3fd58f90899ff23a00af1d0cc3e9788c5995
sizenandosouza <sizenandosilva@gmail.com>
2023-02-13 21:23:44 -0300

    first commit

---
+++ Css/funcoes.js/funcoes.js
@@ -0,0 +1,34 @@
+/*
+      Funçôes: Trecho de código que só é executado quando é chamado/invocado
+
+         Função Void (vazia)
+         Função com Parâmetro(s)
+        Função Return
+        Funçâo Arrow / Arrow Function
+
+*/
+
+
+function soma(numero1, numero2){
+
+      const somaDosNumeros = numero1 + numero2
+
+    return somaDosNumeros
+}
+
+const meusNumeros = soma(4,6)
+
+const multi = (numero1, numero2) => {
+      const multiplicacao = numero1 * numero2
+
+      return multiplicacao
+}
+
+const multiplicacao = multi(2,5)
+
+console.log(multiplicacao)
+
+
+    
+
+
---
+++ Projetos/grid.md
@@ -0,0 +1,13 @@
+#CSS GRID
+
+- Divide a tecla em linhas e colunas
+- Facilita colocar os elementos onde bem querermos
+
+grid-templaste-areas
+-> Define áreas específicas no grid.
+
+grid-templaste-columns
+-> Define o número total de colunas que serão criadas no grid.
+
+grid-templaste-rows
+-> Define a quantidade de linhas do grid.
